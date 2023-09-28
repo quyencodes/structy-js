05b206fa2bf19c10d8e8638a126f81dca39414d3
Quyen Hoang <qduhoang@gmail.com>
2023-09-28 09:27:41 -0700

    maxValue complete

--- 0-intro/maxValue.js
+++ 0-intro/maxValue.js
@@ -1,13 +1,13 @@
-"use strict";
-exports.__esModule = true;
-exports.maxValue = void 0;
-function maxValue(nums) {
-    var res = -Infinity;
-    for (var _i = 0, nums_1 = nums; _i < nums_1.length; _i++) {
-        var num = nums_1[_i];
-        if (res < num)
-            res = num;
-    }
-    return res;
-}
-exports.maxValue = maxValue;
+const maxValue = (nums) => {
+  let max_number = -Infinity;
+
+  for (let num of nums) {
+    if (max_number < num) max_number = num;
+  }
+
+  return max_number;
+};
+
+module.exports = {
+  maxValue,
+};
